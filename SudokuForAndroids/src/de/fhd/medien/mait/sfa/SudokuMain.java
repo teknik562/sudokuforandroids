9556b8a4b8fa7d78e5b28b4cc7fc614f29e73113
painofangels <painofangels@0d8d8eb4-1d4e-0410-b697-5f1131c9a56f>
2008-05-26 18:16:33 +0000

    Initial import.

---
+++ SudokuForAndroids/src/de/fhd/medien/mait/sfa/SudokuMain.java
@@ -0,0 +1,13 @@
+package de.fhd.medien.mait.sfa;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SudokuMain extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+        setContentView(R.layout.main);
+    }
+}
