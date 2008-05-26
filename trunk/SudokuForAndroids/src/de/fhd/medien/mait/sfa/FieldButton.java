9e57b37ed848d17c42a83b08bf089dca94050627
painofangels <painofangels@0d8d8eb4-1d4e-0410-b697-5f1131c9a56f>
2008-05-26 18:33:52 +0000

    Hinzufügen der Klasse FieldButton

---
+++ trunk/SudokuForAndroids/src/de/fhd/medien/mait/sfa/FieldButton.java
@@ -0,0 +1,16 @@
+/**
+ * Diese Klasse repräsentiert ein Sudoku- Feld. 
+ * Die Klasse wird von Button abgeleitet. Zu den Grundfunktionen eines
+ * gewöhnlichen Android- Buttons werden folgende Dinge hinzugefügt:
+ * 			- einen Wert, denn Jedes Sudokufeld hat einen festen Wert
+ * 			- 6 Kandidatenwerte
+ * 			
+ */
+
+
+package de.fhd.medien.mait.sfa;
+import android.widget.Button;
+
+public class FieldButton extends Button{
+
+}
