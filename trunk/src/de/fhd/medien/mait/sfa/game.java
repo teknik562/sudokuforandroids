8db37acf52994b695571cf7f221729248fd93245
ca.ruhr <ca.ruhr@0d8d8eb4-1d4e-0410-b697-5f1131c9a56f>
2008-05-26 08:29:33 +0000

    Kommentar "Bla" hinzugefügt

---
+++ trunk/src/de/fhd/medien/mait/sfa/game.java
@@ -0,0 +1,14 @@
+package de.fhd.medien.mait.sfa;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class game extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+        // Bla
+        setContentView(R.layout.main);
+    }
+}
