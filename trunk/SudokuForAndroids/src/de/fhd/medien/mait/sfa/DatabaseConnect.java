7c8131f0db36859176f6cb8f20995be3756f4f13
ca.ruhr <ca.ruhr@0d8d8eb4-1d4e-0410-b697-5f1131c9a56f>
2008-05-28 10:28:18 +0000

    Initial Import

---
+++ trunk/SudokuForAndroids/src/de/fhd/medien/mait/sfa/DatabaseConnect.java
@@ -0,0 +1,9 @@
+package de.fhd.medien.mait.sfa;
+
+public class DatabaseConnect
+  {
+    private final String DB_NAME = "sudokuForAndroids";
+    private final String DB_USERTABLE = "sfa_user";
+    
+    
+  }
