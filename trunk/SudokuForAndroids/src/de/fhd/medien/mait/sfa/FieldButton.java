b800d40faea0335d70c9a44f6c7f941d5c08e304
jan.monschke <jan.monschke@0d8d8eb4-1d4e-0410-b697-5f1131c9a56f>
2008-06-03 10:34:47 +0000

    Initial import

---
+++ trunk/SudokuForAndroids/src/de/fhd/medien/mait/sfa/FieldButton.java
@@ -0,0 +1,40 @@
+package de.fhd.medien.mait.sfa;
+
+import android.content.Context;
+
+public class FieldButton extends GameButton{
+	
+	private int[] candidateValues = new int[6];
+	
+	/**
+	 * 
+	 * @param context
+	 * @param _value Has to be ONE character only
+	 * @param _size size of the square
+	 * @param _lineSize
+	 * @param _textSize
+	 */
+	public FieldButton(Context context, String _value, int _size, int _lineSize, int _textSize) {
+		super(context, _value, _size, _size, _lineSize, _textSize);
+	}
+	
+	/**
+	 * 
+	 * @param index index of the requestes index
+	 * @return The candidate at the specified index
+	 */
+	public int getCandidate(int index){
+		return candidateValues[index];
+	}
+	
+	/**
+	 * 
+	 * @return An array with the cadidates
+	 */
+	public int[] getCandidates(){
+		return candidateValues;
+	}
+	
+	
+	
+}
