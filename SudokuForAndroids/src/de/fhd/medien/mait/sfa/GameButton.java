be983e7a5725f08274d324626908149d994516d3
jan.monschke <jan.monschke@0d8d8eb4-1d4e-0410-b697-5f1131c9a56f>
2008-06-03 18:34:10 +0000

    Added JavaDoc Minor fixes

--- SudokuForAndroids/src/de/fhd/medien/mait/sfa/GameButton.java
+++ SudokuForAndroids/src/de/fhd/medien/mait/sfa/GameButton.java
@@ -14,9 +14,9 @@ import android.widget.Button;
 	
 public class GameButton extends Button{
 
-	static final int StateDefault = 0;
-	static final int StateFocused = 1;
-	static final int StatePressed = 2;
+	static final int stateDefault = 0;
+	static final int stateFocused = 1;
+	static final int statePressed = 2;
 
 	// Define the colors
 	static final int fillDefault = 0x00000000;
@@ -28,26 +28,74 @@ public class GameButton extends Button{
 	static final int fillFocused = 0;
 	static final int textFocused = 0;
 	
-	static final int fillOriginal = 0;
-	static final int textOriginal = 0;
+	// This variable defines the status of the Button
+	private int mState = stateDefault;
 	
-	static final int fillCandidate = 0;
+	// For each status one Bitmap
+	protected Bitmap defaultBitmap;
+	protected Bitmap focusedBitmap;
+	protected Bitmap pressedBitmap;
 	
-	private int mState = StateDefault;
-	
-	private Bitmap defaultBitmap;
-	private Bitmap focusedBitmap;
-	private Bitmap pressedBitmap;
-	
+	// Basic properties of each button
 	private int height;
 	private int width;
 	private int lineSize;
 	private int textSize;
-
-	private String caption;
-
+	private int radius;
 	
-	private void drawEmptyBitmap(Bitmap current, int color){
+	private String caption;
+	
+	/**
+	 * Creates a simple Button holding a caption
+	 * @param context in best case 'this'
+	 * @param _caption caption to be displayed
+	 * @param _width width of the button
+	 * @param _height height of the button
+	 * @param _lineSize Width of the brush. '2' is best.
+	 * @param _textSize Size of the text. 10 ~ 25 is best.
+	 */
+	public GameButton(Context context, String _caption, int _width, int _height, int _lineSize, int _textSize) {
+		super(context);
+		caption = _caption;
+	    width = _width;
+	    height = _height;
+	    lineSize = _lineSize;
+	    textSize = _textSize;
+	    
+		setClickable(true);
+		
+		// Set the background to delete the original Button image
+		setBackgroundColor(Color.TRANSPARENT);
+		
+		// We now need to reset all steps of a button
+		// 1) default
+		defaultBitmap = Bitmap.createBitmap(width, height, true);
+		// 2) focused
+		focusedBitmap = Bitmap.createBitmap(width, height, true);
+		// 3) pressed
+		pressedBitmap = Bitmap.createBitmap(width, height, true);
+		
+		// Create new Bitmaps for this Button
+		// 1) default
+	    this.drawEmptyBitmap(defaultBitmap, 0xffffffff);
+	    this.drawTextInBitmap(defaultBitmap, 0xff0000ff);
+	    // 2) focused
+	    this.drawEmptyBitmap(focusedBitmap, 0xff97C024);
+	    this.drawTextInBitmap(focusedBitmap, 0xffffffff);
+	    // 3) pressed
+	    this.drawEmptyBitmap(pressedBitmap, 0xff97C024);
+	    this.drawTextInBitmap(pressedBitmap, 0xffffffff);
+	    
+	    // define OnClickListener for the Button
+	    setOnClickListener(onClickListener);
+   	}
+	
+	/**
+	 * Draws a Bitmap for a certain Button
+	 * @param current The status of the Button that should be redrawn
+	 * @param color Button is filled with this color 
+	 */
+	protected void drawEmptyBitmap(Bitmap current, int color){
 		// We need the Canvas object to draw on a Bitmap
 		Canvas canvas = new Canvas();
 		
@@ -75,7 +123,12 @@ public class GameButton extends Button{
 	    canvas.drawPath(path, paint);
 	}
 	
-	private void drawTextInBitmap(Bitmap current, int color){
+	/**
+	 * Draws a text into a certain Button
+	 * @param current The status of the Button that should be redrawn
+	 * @param color Button is filled with this color 
+	 */
+	protected void drawTextInBitmap(Bitmap current, int color){
 		// We need the Canvas object to draw on a Bitmap
 		Canvas canvas = new Canvas();
 		
@@ -87,11 +140,10 @@ public class GameButton extends Button{
 	    paintText.setAntiAlias(true);
 	    paintText.setTextSize(textSize);
 	    paintText.setTextAlign(Paint.Align.CENTER);
-	    paintText.setColor(color);  // white
+	    paintText.setColor(color);
 	   
 	    // Here we calculate the xPos of the text with the help of measureText()
 	    // which gives us the width in px of a certain text
-	    // We need to add 1 and lineSize because of FILL_AND_STROKE
 	    float textXPos = ((width-paintText.measureText(caption) ) / 2);
 	    float textYPos = (paintText.descent()-paintText.ascent()) ;
 	    
@@ -99,63 +151,17 @@ public class GameButton extends Button{
 	    canvas.drawText(caption, textXPos, textYPos, paintText);
 	}
 	
-	/**
-	 * 
-	 * @param context in best case 'this'
-	 * @param _caption caption to be displayed
-	 * @param _width width of the button
-	 * @param _height height of the button
-	 * @param _lineSize Width of the brush. '2' is best.
-	 * @param _textSize Size of the text. 10 ~ 25 is best.
-	 */
-	public GameButton(Context context, String _caption, int _width, int _height, int _lineSize, int _textSize) {
-		super(context);
-		caption = _caption;
-	    width = _width;
-	    height = _height;
-	    lineSize = _lineSize;
-	    textSize = _textSize;
-	    
-		setClickable(true);
-		
-		// Set the background to delete the original Button image
-		setBackgroundColor(Color.TRANSPARENT);
-		
-		// We now need to reset all steps of a button
-		// 1) default
-		defaultBitmap = Bitmap.createBitmap(width, height, true);
-		// 2) fucused
-		focusedBitmap = Bitmap.createBitmap(width, height, true);
-		// 3) pressed
-		pressedBitmap = Bitmap.createBitmap(width, height, true);
-
-	    this.drawEmptyBitmap(defaultBitmap, 0xffffffff);
-	    this.drawTextInBitmap(defaultBitmap, 0xff0000ff);
-	    
-	    this.drawEmptyBitmap(focusedBitmap, 0xff97C024);
-	    this.drawTextInBitmap(focusedBitmap, 0xffffffff);
-	    
-	    this.drawEmptyBitmap(pressedBitmap, 0xff97C024);
-	    this.drawTextInBitmap(pressedBitmap, 0xffffffff);
-	    
-	    // define OnClickListener for the Button
-	    setOnClickListener(onClickListener);
-   	}
-	
 	@Override
 	protected void onDraw(Canvas canvas) {
 		switch (mState) {
-		case StateDefault:
+		case stateDefault:
 			canvas.drawBitmap(defaultBitmap, 0, 0, null);
-//	                   mDebug.append(mCaption + ":default\n");
 			break;
-		case StateFocused:
+		case stateFocused:
 			canvas.drawBitmap(focusedBitmap, 0, 0, null);
-//	                   mDebug.append(mCaption + ":focused\n");
 			break;
-		case StatePressed:
+		case statePressed:
 			canvas.drawBitmap(pressedBitmap, 0, 0, null);
-//	                   mDebug.append(mCaption + ":pressed\n");
 			break;
 		}
 	}
@@ -163,25 +169,29 @@ public class GameButton extends Button{
 	@Override
 	protected void drawableStateChanged() {
 		if (isPressed()) {
-			mState = StatePressed;
+			mState = statePressed;
 		} else if (hasFocus()) {
-			mState = StateFocused;
+			mState = stateFocused;
 		} else {
-			mState = StateDefault;
+			mState = stateDefault;
 		}
-		// force the redraw of the Image
-		// onDraw will be called!
+		// force the redraw of the image
+		// onDraw() will be called
 		invalidate();
 	}
 	
 	private OnClickListener onClickListener =
 		new OnClickListener() {
 		@Override
-		public void onClick(View arg0) {
-//	                   mDebug.append(mCaption + ":click\n");
+		public void onClick(View v) {
+			
 		}
 	};
 	
+	/**
+	 * Returns the caption of the Button
+	 * @return
+	 */
 	public String getCaption(){
 		return this.caption;
 	}
