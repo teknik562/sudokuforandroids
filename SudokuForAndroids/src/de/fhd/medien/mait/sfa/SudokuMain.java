ea72d80535d1d4c25264f31762d21b790326b664
jan.monschke <jan.monschke@0d8d8eb4-1d4e-0410-b697-5f1131c9a56f>
2008-06-05 20:57:48 +0000

    added calculateProperties()

--- SudokuForAndroids/src/de/fhd/medien/mait/sfa/SudokuMain.java
+++ SudokuForAndroids/src/de/fhd/medien/mait/sfa/SudokuMain.java
@@ -3,6 +3,8 @@ package de.fhd.medien.mait.sfa;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Display;
+import android.view.WindowManager;
 
 public class SudokuMain extends Activity {
     /** Called when the activity is first created. */
@@ -10,12 +12,51 @@ public class SudokuMain extends Activity {
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
         
+        // get the info from the display and calculate layout
+        calculateProperties();
+        
         Intent next = new Intent(this, CreateUser.class);
         next.putExtra("USERNAME", "Hans");
         
         this.startSubActivity(next, 0);
         
         
+        
         setContentView(R.layout.main);
     }
+    
+    /** 
+     *  Calculates layout properties and stores them in the config.
+     */ 
+    private void calculateProperties(){
+    	WindowManager a = this.getWindowManager();
+        Display v = a.getDefaultDisplay();
+        
+        // calculates the minimumscreenAttr
+        int min = Math.min(v.getHeight(),v.getWidth());
+        if (min == v.getHeight())
+        	min = min - 50;
+        
+        // calculates the Font Size for Fieldbuttons
+        // if the minimum ScreenAttr is less 250 it will be 12
+        // otherwise it'll be 16
+        int FBfontSize;
+        if (min < 250)
+        	FBfontSize = 12;
+        else
+        	FBfontSize = 16;
+        
+        Config.initiate(v.getWidth(),			// displayWidth
+        		v.getHeight() - 50,				// displayHeight
+        		min,							// minScreenAttr
+        		min / 10,						// optFieldBtSize 
+        		min / 40,						// optFieldPadding
+        		min / 120,						// optFieldLine
+        		9 * (min / 10) + 2 * (min / 40),// fieldLength
+        		(v.getWidth() - (9 * (min / 10) + 2 * (min / 40)) ) / 2,	// fieldStartXPos
+        		((v.getHeight() - 50) - (9 * (min / 10) + 2 * (min / 40)) ) / 2,	// fieldStartYPos
+        		FBfontSize						// optFieldBtFontSize
+        		);
+    }
+    
 }
