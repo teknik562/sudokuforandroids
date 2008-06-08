109f8026fa5217dfad3e924271a3d7bed2b4f7b2
jan.monschke <jan.monschke@0d8d8eb4-1d4e-0410-b697-5f1131c9a56f>
2008-06-08 12:05:17 +0000

    now the main menu is shown I commented the DB code which now can be removed to another class

--- SudokuForAndroids/src/de/fhd/medien/mait/sfa/SudokuMain.java
+++ SudokuForAndroids/src/de/fhd/medien/mait/sfa/SudokuMain.java
@@ -4,9 +4,27 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Display;
+import android.view.View;
 import android.view.WindowManager;
+import android.view.View.OnClickListener;
+import android.widget.AbsoluteLayout;
+import android.widget.LinearLayout.LayoutParams;
 
 public class SudokuMain extends Activity {
+	/** Background color */
+	private static final int fillBgMenu = 0x1007C024;
+	
+	// Main layout
+	AbsoluteLayout absLayoutMenu;
+	
+	// Buttons
+	GameButton newGameBt;
+	GameButton contGameBt;
+	GameButton settingsBt;
+	GameButton highscoreBt;
+	GameButton helpBt;
+	GameButton creditsBt;
+	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle icicle) {
@@ -15,6 +33,24 @@ public class SudokuMain extends Activity {
         // get the info from the display and calculate layout
         calculateProperties();
         
+        // initiate layout
+        absLayoutMenu = new AbsoluteLayout(this);
+        absLayoutMenu.setLayoutParams(new AbsoluteLayout.LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.FILL_PARENT, 0, 0));
+        absLayoutMenu.setBackgroundColor(fillBgMenu);
+
+        // create & place Buttons on layout
+        createButtons();
+        placeButtons();
+        
+        // set the onClickListeners
+        helpBt.setOnClickListener(helpClick);
+        creditsBt.setOnClickListener(creditsClick);
+        
+        setContentView(absLayoutMenu);
+        
+        
+        
+        /*
         Intent next = new Intent(this, CreateUser.class);
         next.putExtra("USERNAME", "Hans");
         
@@ -23,7 +59,85 @@ public class SudokuMain extends Activity {
         
         
         setContentView(R.layout.main);
+        */
     }
+    
+    /**
+     * Creates Buttons for the main menu
+     */
+    private void createButtons(){
+    	newGameBt = new GameButton(this, getString(R.string.newGame), Config.optMenuBtWidth,
+        		Config.optMenuBtHeight, Config.lineSize, Config.FontSize);
+        
+        
+        contGameBt = new GameButton(this, getString(R.string.contGame), Config.optMenuBtWidth,
+        		Config.optMenuBtHeight, Config.lineSize, Config.FontSize);
+        
+        
+        settingsBt = new GameButton(this, getString(R.string.settings), Config.optMenuBtWidth,
+        		Config.optMenuBtHeight, Config.lineSize, Config.FontSize);
+        
+        
+        highscoreBt = new GameButton(this, getString(R.string.highscore), Config.optMenuBtWidth,
+        		Config.optMenuBtHeight, Config.lineSize, Config.FontSize);
+        
+        
+        helpBt = new GameButton(this, getString(R.string.help), Config.optMenuBtWidth,
+        		Config.optMenuBtHeight, Config.lineSize, Config.FontSize);
+        
+        
+        creditsBt = new GameButton(this, getString(R.string.credits), Config.optMenuBtWidth,
+        		Config.optMenuBtHeight, Config.lineSize, Config.FontSize);
+        
+    }
+    
+    /**
+     * Places buttons from main manu on the main layout
+     */
+    private void placeButtons(){
+    absLayoutMenu.addView(newGameBt, 
+			new AbsoluteLayout.LayoutParams(Config.optMenuBtWidth,
+					Config.optMenuBtHeight, Config.menutStartXPos, 10));
+    
+    absLayoutMenu.addView(contGameBt, 
+			new AbsoluteLayout.LayoutParams(Config.optMenuBtWidth,
+					Config.optMenuBtHeight, Config.menutStartXPos, 10 + 1 * Config.optMenuBtHeight));
+    
+    absLayoutMenu.addView(settingsBt, 
+			new AbsoluteLayout.LayoutParams(Config.optMenuBtWidth,
+					Config.optMenuBtHeight, Config.menutStartXPos, 10 + 2 * Config.optMenuBtHeight));
+    
+    absLayoutMenu.addView(highscoreBt, 
+			new AbsoluteLayout.LayoutParams(Config.optMenuBtWidth,
+					Config.optMenuBtHeight, Config.menutStartXPos, 10 + 3 * Config.optMenuBtHeight));
+    
+    absLayoutMenu.addView(helpBt, 
+			new AbsoluteLayout.LayoutParams(Config.optMenuBtWidth,
+					Config.optMenuBtHeight, Config.menutStartXPos, 10 + 4 * Config.optMenuBtHeight));
+    
+    absLayoutMenu.addView(creditsBt, 
+			new AbsoluteLayout.LayoutParams(Config.optMenuBtWidth,
+					Config.optMenuBtHeight, Config.menutStartXPos, 10 + 5 * Config.optMenuBtHeight));
+    }
+    
+    /** When clicked on Credits a new View is opened where the participants are shown */
+    OnClickListener creditsClick = new OnClickListener(){
+        //@Override
+        	public void onClick(View v) {
+        		Intent showCredits = new Intent(SudokuMain.this, Credits.class);
+				startActivity(showCredits);
+        	}	
+    };
+    
+    /** When clicked on Help a new View is opened where the user is shown how to use this app */
+    OnClickListener helpClick = new OnClickListener(){
+        //@Override
+        	public void onClick(View v) {
+        		Intent showHelp = new Intent(SudokuMain.this, Help.class);
+				startActivity(showHelp);
+        	}	
+    };
+    
     
     /** 
      *  Calculates layout properties and stores them in the config.
@@ -37,7 +151,7 @@ public class SudokuMain extends Activity {
         if (min == v.getHeight())
         	min = min - 50;
         
-        // calculates the Font Size for Fieldbuttons
+        // calculates the Font Size
         // if the minimum ScreenAttr is less 250 it will be 12
         // otherwise it'll be 16
         int FBfontSize;
@@ -55,7 +169,10 @@ public class SudokuMain extends Activity {
         		9 * (min / 10) + 2 * (min / 40),// fieldLength
         		(v.getWidth() - (9 * (min / 10) + 2 * (min / 40)) ) / 2,	// fieldStartXPos
         		((v.getHeight() - 50) - (9 * (min / 10) + 2 * (min / 40)) ) / 2,	// fieldStartYPos
-        		FBfontSize						// optFieldBtFontSize
+        		FBfontSize,						// FontSize
+        		min / 8,						// optMenuBtHeight
+        		min / 2,						// optMenuBtWidth
+        		min / 4							// menutStartXPos						
         		);
     }
     
