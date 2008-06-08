7c67761af9d822e71a2269be497f93d3cb2cb12b
jan.monschke <jan.monschke@0d8d8eb4-1d4e-0410-b697-5f1131c9a56f>
2008-06-08 12:03:21 +0000

    initial import

---
+++ SudokuForAndroids/src/de/fhd/medien/mait/sfa/Credits.java
@@ -0,0 +1,15 @@
+package de.fhd.medien.mait.sfa;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Credits extends Activity{
+	
+	
+	/** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle icicle) {
+    	super.onCreate(icicle);
+    	setContentView(R.layout.credits);
+    }
+}
