3afa2602611f5e658520949b55d340c56997c041
jan.monschke <jan.monschke@0d8d8eb4-1d4e-0410-b697-5f1131c9a56f>
2008-06-27 16:32:08 +0000

    added time information

--- SudokuForAndroids/src/de/fhd/medien/mait/sfa/Config.java
+++ SudokuForAndroids/src/de/fhd/medien/mait/sfa/Config.java
@@ -11,41 +11,44 @@ package de.fhd.medien.mait.sfa;
  */
 public class Config {
 	/** Width of the display */
-	public static int displayWidth = 0;
+	static int displayWidth = 0;
 	/** Height of the display (minus the 50px of the Android panel) */
-	public static int displayHeight = 1;
+	static int displayHeight = 1;
 	/** The minimum screen attribute which could be 
 	 * either the height (minus the 50 px) or the width */
-	public static int minScreenAttr = 2;
+	static int minScreenAttr = 2;
 	/** Calulated size of the FieldButtons */
-	public static int optFieldBtSize = 3;
+	static int optFieldBtSize = 3;
 	/** Calculated padding between each 3 buttons (horizontally or vertically) */
-	public static int optFieldPadding = 4;
+	static int optFieldPadding = 4;
 	/** Calculated width of a spacing line between each block of 3*3 */
-	public static int optFieldLine = 5;
+	static int optFieldLine = 5;
 	/** Length of the GamePanel */
-	public static int fieldLength = 6;
+	static int fieldLength = 6;
 	/** Calculated x start position for the field */
-	public static int fieldStartXPos = 7;
+	static int fieldStartXPos = 7;
 	/** Calculated y start position for the field */
-	public static int fieldStartYPos = 8;
+	static int fieldStartYPos = 8;
 	/** Calculated best font size */
-	public static int FontSize = 9;
+	static int FontSize = 9;
 	/** Calculated height of menu buttons */
-	public static int optMenuBtHeight = 10;
+	static int optMenuBtHeight = 10;
 	/** Calculated width of menu buttons */
-	public static int optMenuBtWidth = 11;
+	static int optMenuBtWidth = 11;
 	/** Calculated start x of the menu button */
-	public static int menutStartXPos = 12;
+	static int menutStartXPos = 12;
 	/** saves the name of the player */
-	public static String playerName = "";
+	static String playerName = "";
 	/** Saves the chosen difficulty */
-	public static int difficulty;
-	
+	static int difficulty;
+	/** stores the time the game was started */
+	static long startTime = 0;
+	/** stores the time needed until now*/
+	static long neededTime = 0;
 	/** Best linesize to draw buttons */
-	public static final int lineSize = 2;
+	static final int lineSize = 2;
 	
-	public static void initiate(int _displayWidth, int _displayHeight, int _minScreenAttr,
+	static void initiate(int _displayWidth, int _displayHeight, int _minScreenAttr,
 			int _optFieldBtSize, int _optFieldPadding, int _optFieldLine, int _fieldLength,
 			int _fieldStartXPos, int _fieldStartYPos , int _FontSize, int _optMenuBtHeight,
 			int _optMenuBtWidth, int _menutStartXPos, String _playerName){
