f52c790957134d20f1d5e93bfdb41b1153906eff
jan.monschke <jan.monschke@0d8d8eb4-1d4e-0410-b697-5f1131c9a56f>
2008-06-27 16:35:46 +0000

    changed loading sequence and added a self updating adapter

--- SudokuForAndroids/src/de/fhd/medien/mait/sfa/Load.java
+++ SudokuForAndroids/src/de/fhd/medien/mait/sfa/Load.java
@@ -1,24 +1,153 @@
 package de.fhd.medien.mait.sfa;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 import android.app.ListActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
+/**
+ * This activity displays all savegames by the user and lets him choose one to go on playing.
+ * If there's no savegame by the player only a message will be shown.
+ * @author thedeftone
+ *
+ */
 public class Load extends ListActivity{
-	
-	String[] directory;
 
 	/** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle); 
-        
+    String[] directory;   
 	File f = new File("/data/data/de.fhd.medien.mait.sfa/files");
 	directory = f.list();
 	
-	this.setListAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, this.directory));
+	// Only savegames by the player will be shown, others are hided
+	ArrayList<String> tempDirectory = new ArrayList<String>();
+	for(String element : directory){
+		// Savegames to be displayed must start with "Playername "
+		if(element.startsWith(Config.playerName + " "))
+			tempDirectory.add(element);
+	}
+	String[] userDirectory = new String[tempDirectory.size()];
+	tempDirectory.toArray(userDirectory);
 	
+	// If there are no savegames, a message will be shown
+	if (userDirectory.length == 0){
+		Toast.makeText(this, "There are no savegames from you", Toast.LENGTH_SHORT).show();
+		try{
+			this.wait(Toast.LENGTH_SHORT);
+			finish();
+		}catch(Exception e){
+			finish();
+		}
+	// if there are savegames by this user, a list will be shown
+	}else{
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, userDirectory);
+		this.setListAdapter(adapter);
+	}
+    }
+    
+    
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id){
+     super.onListItemClick(l, v, position, id); 
+     
+     // Get the item that was clicked
+     Object o = this.getListAdapter().getItem(position);
+     String fileName = o.toString();
+     Log.d("Chosen Savegame", fileName);
+     try{
+         BufferedReader br = 	new BufferedReader(
+        		 				new InputStreamReader(
+        		 				openFileInput(fileName)));    	 
+    	 // load all liness
+         String solvedFieldString = br.readLine();
+    	 String originalFieldString = br.readLine();
+    	 String userManipulatedFieldString = br.readLine();
+    	 String candidateString = br.readLine();
+    	 String nameString = br.readLine();
+    	 String levelString = br.readLine();
+    	 String sumTimeString = br.readLine();
+    	 
+    	 br.close();
+    	 //Config.loadedScore = loadedObject;
+    	 Intent loadGame = new Intent(Load.this, Field.class);
+    	 loadGame.putExtra("starter", "LOAD");
+    	 
+    	 // pass the Strings to the field
+    	 loadGame.putExtra("sfs", solvedFieldString);
+    	 loadGame.putExtra("ofs", originalFieldString);
+    	 loadGame.putExtra("umfs", userManipulatedFieldString);
+    	 loadGame.putExtra("cs", candidateString);
+    	 loadGame.putExtra("ns", nameString);
+    	 loadGame.putExtra("ls", levelString);
+    	 loadGame.putExtra("sts", sumTimeString);
+    	 loadGame.putExtra("fileName", fileName);
+    	 
+    	 Log.d("Loading", "Loading successful");
+    	 startActivity(loadGame);
+    	 this.finish();
+     } catch(Exception e){
+    	 Log.d("Loading", "Loading failed");
+     }
+     
+    }
+    
+    /**
+     * This method creates the Otions-Menu
+     */
+    public boolean onCreateOptionsMenu(final Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        
+        menu.add(0, 1, "Delete selected savegame");
+        
+        return true;
+    }
+    
+    /**
+     * This method handles clicks on items of the options-menu
+     */
+    public boolean onOptionsItemSelected(Menu.Item item){
+        
+        if(item.getId() == 1){
+        	try{
+        		String chosenFile = this.getListView().getSelectedItem().toString();
+        		if (deleteFile(chosenFile))
+        			Toast.makeText(this, chosenFile + " deleted", Toast.LENGTH_SHORT).show();
+        		else
+        			Toast.makeText(this, "Can't delete that file", Toast.LENGTH_SHORT).show();
+        	}catch(Exception e){
+        		Toast.makeText(this, "You need to choose a file first", Toast.LENGTH_SHORT).show();
+        	}
+        }
+        
+        String[] directory;   
+    	File f = new File("/data/data/de.fhd.medien.mait.sfa/files");
+    	directory = f.list();
+    	
+    	// Only savegames by the player will be shown, others are hided
+    	ArrayList<String> tempDirectory = new ArrayList<String>();
+    	for(String element : directory){
+    		// Savegames to be displayed must start with "Playername "
+    		if(element.startsWith(Config.playerName + " "))
+    			tempDirectory.add(element);
+    	}
+    	String[] userDirectory = new String[tempDirectory.size()];
+    	tempDirectory.toArray(userDirectory);
+    	
+    	ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, userDirectory);
+    	this.setListAdapter(adapter);
+
+        return true;
     }
 }
