ba002c4a2d3770bbc7758bc87ad6f73276b6c597
jan.monschke <jan.monschke@0d8d8eb4-1d4e-0410-b697-5f1131c9a56f>
2008-06-27 16:38:07 +0000

    saving and loading now works perfectly

--- trunk/SudokuForAndroids/src/de/fhd/medien/mait/sfa/Field.java
+++ trunk/SudokuForAndroids/src/de/fhd/medien/mait/sfa/Field.java
@@ -1,17 +1,23 @@
 package de.fhd.medien.mait.sfa;
 
 
+import java.io.File;
+import java.io.OutputStreamWriter;
+import java.util.Date;
+
 import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.DateFormat;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.AbsoluteLayout;
+import android.widget.Toast;
 import android.widget.LinearLayout.LayoutParams;
 
 /**
@@ -23,54 +29,127 @@ public class Field extends Activity{
 	//the code to identify the Keypad, when it returns to this activity
 	private static final int KEYPAD_REQUEST_CODE = 0;
 	
+	// Shows wether the game was loaded or not
+	private boolean gameWasLoaded = false; 
+	// File Name of the loaded File
+	private String fileNameloaded = "";
+	
 	// Layout for the field
 	private AbsoluteLayout fieldLayout = new AbsoluteLayout(this);
 	private static final int length = 9;
+	private static final int nrCandidates = 6;
 	private FieldButton[][] buttonField = new FieldButton[length][length];
-	int[][] testarray = {
-			{0, 0, 0, 4, 0, 0, 8, 3, 9},
-			{5, 0, 3, 4, 0, 0, 8, 0, 9},
-			{0, 0, 3, 4, 1, 7, 0, 3, 9},
-			{0, 0, 3, 0, 1, 0, 8, 0, 9},
-			{2, 0, 3, 4, 1, 7, 8, 3, 0},
-			{0, 0, 3, 4, 0, 7, 0, 3, 9},
-			{0, 0, 0, 0, 1, 7, 8, 3, 9},
-			{0, 0, 3, 4, 1, 7, 8, 0, 9},
-			{0, 0, 0, 4, 1, 7, 8, 3, 9}
-	};
+	/** represents the Field with non changeable values */
+	int[][] originalField = new int[length][length];
+	/** represents the Field with the right solution */
+	private int[][] solvedField = new int[length][length];
+	/** 
+	 * represents the Field the user has manipulated.
+	 * If this field is equal to solvedField, the game is over.
+	 */
+	private int[][] userManipulatedField = new int[length][length];
+	
+	
+	
 	ProgressDialog pd = null; 
 	
-	Thread t = new Thread()
+	Thread normal = new Thread()
     {
            public void run()
            {
         	   // request a puzzle basing on the set difficulty
+        	   Algo normal = new Algo();
+        	   normal.Algo(Config.difficulty);
+        	   userManipulatedField = normal.getMaskedField();
+        	   originalField = normal.getMaskedField();
+        	   solvedField = normal.getFilledField();
         	   // create the field
+        	   
                createField();
                for(int x = 0; x < buttonField.length;  x++)
                	for(int y = 0; y < buttonField[x].length; y++)
-               		if(buttonField[x][y].changeable == true)
+               		if(buttonField[x][y].changeable == true){
                			buttonField[x][y].setOnClickListener(fieldClick);
+               		}
                
                //when done
         	   pd.dismiss(); 
            }
 
     }; 
+    
+    Thread loaded = new Thread()
+    {
+           public void run()
+           {
+        	   Config.playerName = getIntent().getStringExtra("ns");
+        	   Config.difficulty = Integer.parseInt(getIntent().getStringExtra("ls"));
+        	   Config.neededTime = Long.parseLong(getIntent().getStringExtra("sts"));
+        	   String solvedFieldString = getIntent().getStringExtra("sfs");
+        	   String originalFieldString = getIntent().getStringExtra("ofs");
+        	   String userManipulatedFieldString = getIntent().getStringExtra("umfs");
+        	   Log.d("OFS", originalFieldString);
+        	   int counter = 0;
+        	   for(int i = 0; i < length; i++)
+        		   for(int j = 0; j < length; j++){
+        			   solvedField[i][j] = Character.getNumericValue(solvedFieldString.charAt(counter));
+        			   originalField[i][j] = Character.getNumericValue(originalFieldString.charAt(counter));
+        			   userManipulatedField[i][j] = Character.getNumericValue(userManipulatedFieldString.charAt(counter));
+        			   counter++;
+        		   }
+        	   
+        			   
+        	   // request a puzzle basing on the set difficulty
+        	   // create the field
+               createField();
+               for(int x = 0; x < buttonField.length;  x++)
+               	for(int y = 0; y < buttonField[x].length; y++){
+               		buttonField[x][y].setValue(userManipulatedField[x][y]);
+               		if(buttonField[x][y].changeable == true){
+               			buttonField[x][y].setOnClickListener(fieldClick);
+               		}
+               	}
+               
+               //when done
+        	   pd.dismiss(); 
+           }
+
+    }; 
+    
 	
 	/** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle); 
         
+        // set the start time
+        Config.startTime = new Date().getTime();
+        
+        String starter = "NORMAL";
+        try{
+        	starter = this.getIntent().getStringExtra("starter");
+        } catch(Exception e){
+        	Toast.makeText(this, e.getClass().toString(), Toast.LENGTH_SHORT).show();
+        }
+
+        pd = ProgressDialog.show(Field.this,     
+                "Please wait...", "Generating Sudoku", true);    
+    
         // initiate the Layout
         fieldLayout.setLayoutParams(new AbsoluteLayout.LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.FILL_PARENT, 0, 0));
-        pd = ProgressDialog.show(Field.this,     
-                "Please wait...", "Generating Sudoku", true);
+        
         Handler handler = new Handler(); 
-        handler.post(t);
+        
+        if(starter.equals("NORMAL"))
+        	handler.post(normal);
+        else{
+        	fileNameloaded = getIntent().getStringExtra("fileName");
+        	gameWasLoaded = true;
+        	handler.post(loaded);
+        }
+        
         Log.d("Chosen difficulty", Integer.toString(Config.difficulty));
-        	
+        Log.d("Mode", starter + " Mode");	
         // request a puzzle basing on the set difficulty
 
         
@@ -219,12 +298,12 @@ public class Field extends Activity{
         		// a new button is created at the given location
         		// testarray will be deleted soon
         		String caption = "";
-        		if(testarray[i][j] != 0)
-        			caption = Integer.toString(testarray[i][j]);
+        		if(originalField[i][j] != 0)
+        			caption = Integer.toString(originalField[i][j]);
         		buttonField[i][j] = new FieldButton(this, caption, Config.optFieldBtSize, 2, Config.FontSize, false);
         		// when there's a ressource from the algo here will be a 
         		// decision whether the button is a normal button or a noChangeButton
-        		if(testarray[i][j] != 0)
+        		if(originalField[i][j] != 0)
         			buttonField[i][j].setAsNoChange();
         		// horizontal padding is set
         		if(j%3 == 0 && j != 0)
@@ -297,6 +376,136 @@ public class Field extends Activity{
     	    		
 	}//end method
 
+    private void saveGame(){
+    	int[][][] candidates = getCandidates();
+
+    	String solvedFieldString = "";
+    	String userManipulatedField = "";
+    	String candidateString = "";
+    	for (int i = 0; i < length; i++){
+    		for (int j = 0; j < length; j++){
+    			userManipulatedField += buttonField[i][j].getValue();
+    			solvedFieldString += Integer.toString(solvedField[i][j]);
+    			for(int k = 0; k < nrCandidates; k++)
+    				if (candidates[i][j][k] != 0)
+    					candidateString += candidates[i][j][k];
+    				else
+    					candidateString += "0";
+    		}
+    	}
+    	
+    	String originalField = "";
+    	for (int i = 0; i < length; i++){
+    		for (int j = 0; j < length; j++){
+    			if (buttonField[i][j].getCaption() == "" ||  
+    				Integer.toString(buttonField[i][j].getValue()).equals(buttonField[i][j].getCaption()))
+    				originalField += "0";
+    			else
+    				originalField += buttonField[i][j].getCaption();
+    		}
+    	}
+
+    	try{
+        	Date d = new Date();
+        	// Date looks like: 12.28.08
+        	String date = DateFormat.format("MM.dd.yy",d.getTime()).toString();
+        	String fileName = Config.playerName + "   " + date;
+        	
+        	if(gameWasLoaded){
+        		fileName = fileNameloaded;
+        		Log.d("File", "no need to rename, it was loaded");
+        	}
+        	else{
+        	int i = 1;
+        	boolean fileExists = false;
+        	File g = new File("/data/data/de.fhd.medien.mait.sfa/files");
+        	String[] directory = g.list();
+        	for(String s : directory){
+        		if(s.equals(fileName)){
+        			Log.d("First check on File", "exists already: " + fileName);
+        			fileExists = true;
+        		}
+        		else{
+        			Log.d("First check on File", "Doesn't exist: " + fileName);
+        		}
+        	}
+        	String fileEnding = "";
+        	while(fileExists){
+        		i++;
+        		fileEnding = "(" + Integer.toString(i) + ")";
+        		Log.d("File", "Changed fileName to: ..." + fileEnding);
+        		boolean found = false;
+        		for(String s : directory){
+            		if(s.endsWith(fileEnding)){
+            			Log.d("File", "exists already: ..." + fileEnding);
+            			fileExists = true;
+            			found = true;
+            		}
+            		else{
+            			if(found == false){
+            				Log.d("File", "Doesn't exist: " + fileEnding);
+            				fileExists = false;
+            			}
+            		}
+            	}
+        		
+        	}
+        	fileName += fileEnding;
+        	}
+    		OutputStreamWriter fw = new OutputStreamWriter(
+					openFileOutput(fileName, MODE_WORLD_READABLE));
+    		// write solved field
+    		fw.write(solvedFieldString);
+    		// separate lines
+    		fw.append((char)Character.LINE_SEPARATOR);
+    		// write original field
+    		fw.append(originalField);
+    		// separate lines
+    		fw.append((char)Character.LINE_SEPARATOR);
+    		// write user maipulated field
+    		fw.append(userManipulatedField);
+    		// separate lines
+    		fw.append((char)Character.LINE_SEPARATOR);
+    		// write candidates
+    		fw.append(candidateString);
+    		// separate lines
+    		fw.append((char)Character.LINE_SEPARATOR);
+    		// write player name
+    		fw.append(Config.playerName);
+    		// separate lines
+    		fw.append((char)Character.LINE_SEPARATOR);
+    		// write level
+    		fw.append(Integer.toString(Config.difficulty));
+    		// separate lines
+    		fw.append((char)Character.LINE_SEPARATOR);
+    		// write time needed until now
+    		long now = new Date().getTime();
+    		long needed = now - Config.startTime;
+    		fw.append(Long.toString(needed));
+    		fw.flush();
+    		fw.close();
+    	
+    	Log.d("Saving...", "Passed");
+    	Toast.makeText(this, "Game saved", Toast.LENGTH_SHORT).show();
+    	} catch(Exception ex){
+    		Log.d("Saving...", "Failed" + ex.fillInStackTrace());
+    		Toast.makeText(this, "Game not saved, try again", Toast.LENGTH_SHORT).show();
+    	}
+    }
+    
+    private int[][][] getCandidates(){
+    	int[][][] candidates = new int[length][length][nrCandidates];
+    	for(int i = 0; i < length; i++){
+    		for(int j = 0; j < length; j++){
+    			int[] temp = buttonField[i][j].getCandidates();
+    			for(int k = 0; k < nrCandidates; k++){
+    				candidates[i][j][k] = temp[k];
+    			}
+    		}
+    	}
+    	return candidates;
+    }
+    
     /**
      * This method creates the Otions-Menu
      */
@@ -304,7 +513,7 @@ public class Field extends Activity{
       {
         super.onCreateOptionsMenu(menu);
         
-        menu.add(0, 1, "Spiel speichern");
+        menu.add(0, 1, "Save Game");
         menu.add(0, 2, "Spiel fertig");
         
         return true;
@@ -318,6 +527,7 @@ public class Field extends Activity{
         // go to input form
         if(item.getId() == 1)
           {
+        	saveGame();
           }
         if(item.getId() == 2)
           {
@@ -328,6 +538,11 @@ public class Field extends Activity{
         return true;
       }
    
+//    protected void onDestroy(){
+//    	saveGame();
+//    	super.onDestroy();
+//    }
+    
  /**
   * this method gives information, if there is at least one field, on which the 
   * focus lays at the moment
