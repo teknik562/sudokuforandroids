f2416a501112bcd6b6d9ea6981160e71efc7af80
jan.monschke <jan.monschke@0d8d8eb4-1d4e-0410-b697-5f1131c9a56f>
2008-07-07 10:31:38 +0000

    added icons in menu

--- trunk/SudokuForAndroids/src/de/fhd/medien/mait/sfa/UserList.java
+++ trunk/SudokuForAndroids/src/de/fhd/medien/mait/sfa/UserList.java
@@ -105,7 +105,7 @@ public class UserList extends ListActivity
         {
           super.onCreateOptionsMenu(menu);
           
-          menu.add(0, 1, "New Player");
+          menu.add(0, 1, "New Player").setIcon(R.drawable.adduser);
           
           return true;
         }
